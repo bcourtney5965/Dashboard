79e913df0cbacbf641d0087ed6e0f63d99cae325
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-07-06 19:30:52 -0600

    DashboardLayout component renders all sections

--- __tests__/DashboardLayout.test.tsx
+++ __tests__/DashboardLayout.test.tsx
@@ -1,16 +1,49 @@
 import '@testing-library/jest-dom'
-// import {render, screen} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import DashboardLayout from '../app/home/DashboardLayout'
 
 describe('DashboardLayout', () => {
   it('returns a function', () => {
     expect(DashboardLayout).toBeInstanceOf(Function)
   })
-  // it('renders a heading', () => {
-  //   render(<Page />)
 
-  //   const heading = screen.getByRole('heading', {level: 1})
+  it('renders all sections', () => {
+    render(<DashboardLayout />)
 
-  //   expect(heading).toBeInTheDocument()
-  // })
+    const income = screen.getByRole('heading', {
+      name: 'Income:',
+      level: 2,
+    })
+    expect(income).toBeInTheDocument()
+
+    const passiveIncome = screen.getByText('Passive Income:')
+    expect(passiveIncome).toBeInTheDocument()
+
+    const netWorth = screen.getByText('Net Worth')
+    expect(netWorth).toBeInTheDocument()
+
+    const marginalTaxRate = screen.getByText('Marginal Tax Rate:')
+    expect(marginalTaxRate).toBeInTheDocument()
+
+    const capitalGainsTaxRate = screen.getByText('Capital Gains Tax Rate:')
+    expect(capitalGainsTaxRate).toBeInTheDocument()
+
+    const investmentsSection = screen.getByRole('heading', {
+      name: 'Investments',
+      level: 1,
+    })
+    expect(investmentsSection).toBeInTheDocument()
+
+    const passiveIncomeSection = screen.getByRole('heading', {
+      name: 'Passive Income',
+      level: 1,
+    })
+    expect(passiveIncomeSection).toBeInTheDocument()
+
+    const incomeSection = screen.getByRole('heading', {
+      name: 'Income',
+      level: 1,
+    })
+    expect(incomeSection).toBeInTheDocument()
+  })
 })
--- app/home/DashboardLayout.tsx
+++ app/home/DashboardLayout.tsx
@@ -3,7 +3,30 @@ import React from 'react'
 function DashboardLayout() {
   return (
     <div>
-      <h1>Dashboard Layout</h1>
+      <section>
+        <h2>Income:</h2>
+      </section>
+      <section>
+        <h2>Passive Income:</h2>
+      </section>
+      <section>
+        <h2>Net Worth</h2>
+      </section>
+      <section>
+        <h2>Marginal Tax Rate:</h2>
+      </section>
+      <section>
+        <h2>Capital Gains Tax Rate:</h2>
+      </section>
+      <section>
+        <h1>Investments</h1>
+      </section>
+      <section>
+        <h1>Passive Income</h1>
+      </section>
+      <section>
+        <h1>Income</h1>
+      </section>
     </div>
   )
 }
