608c759c53ef354495bc1ffe3190358b80e9e4c4
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-07-16 13:28:56 -0600

    Configure Mantine & Tailwind to coexist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,39 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
+// Mantine styles
+import '@mantine/core/styles.layer.css'
+import '@mantine/charts/styles.layer.css'
+import '@mantine/code-highlight/styles.layer.css'
+import '@mantine/core/styles.layer.css'
+import '@mantine/dates/styles.layer.css'
+import '@mantine/notifications/styles.layer.css'
+import '@mantine/nprogress/styles.layer.css'
+import '@mantine/spotlight/styles.layer.css'
+
+// imports Tailwind CSS
+import './globals.css'
+
+import {ColorSchemeScript, MantineProvider} from '@mantine/core'
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : 'http://localhost:3000'
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
+  title: 'Dashboard',
+  description: 'For tracking income, passive income, net worth, and tax rates',
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
-          {children}
+          <MantineProvider>{children}</MantineProvider>
         </main>
       </body>
     </html>
-  );
+  )
 }
